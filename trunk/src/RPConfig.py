2ddd03cc624865a3b0edd351a521ac038efd96b0
rbulha@gmail.com <rbulha@gmail.com@3db46129-f7cc-561c-f858-d950435ae609>
2010-05-28 18:25:44 +0000

    first seed

---
+++ trunk/src/RPConfig.py
@@ -0,0 +1,10 @@
+class CRPConfig:
+    C_H_NORMAL    = 9.1
+    C_H_SEXTA     = 7.6
+    T_ALMOCO      = 1.5
+    H_E_OFICIAL   = 7.0
+    H_S_OFICIAL   = 17.6
+    H_S_OFICIAL_SEXTA = 16.1     
+    H_S_ALMOCO    = 11.6
+    H_E_ALMOCO    = 13.1    
+    
