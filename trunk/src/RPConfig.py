3e46d8eb52c85c4550ec46c7f41bce5f03a074ce
rbulha@gmail.com <rbulha@gmail.com@3db46129-f7cc-561c-f858-d950435ae609>
2011-01-10 13:14:05 +0000

    get time in seconds added

--- trunk/src/RPConfig.py
+++ trunk/src/RPConfig.py
@@ -1,3 +1,5 @@
+import time
+
 class CRPConfig:
     C_H_NORMAL    = 9.1
     C_H_SEXTA     = 7.6
@@ -6,5 +8,11 @@ class CRPConfig:
     H_S_OFICIAL   = 17.6
     H_S_OFICIAL_SEXTA = 16.1     
     H_S_ALMOCO    = 11.6
-    H_E_ALMOCO    = 13.1    
-    
+    H_E_ALMOCO    = 13.1  
+    @staticmethod  
+    def GetJorneyInSeconds():
+        nowtime = time.localtime()
+        if nowtime.tm_wday == 4: #Sexta-feira
+            return 7*3600 + 36*60
+        else:
+            return 9*3600 + 6*60
