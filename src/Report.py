cfae5134f2f5fe19d8173237f52d5b0f6d90eea0
rbulha <rbulha@gmail.com>
2011-01-11 13:19:50 +0000

    report statistics almost ready

--- src/Report.py
+++ src/Report.py
@@ -4,6 +4,11 @@ Created on 10/01/2011
 @author: rogerio_bulha
 '''
 import wx 
+import time
+
+from datetime import date
+from datetime import timedelta
+from datetime import datetime
 
 from GUI_xrc import xrcCReportFrame
 
@@ -16,6 +21,13 @@ class CReportVirtualList(wx.ListCtrl):
 
         self.parent = parent
         self.cPontDB = PontoDB
+        
+        self.week_day = ['MON','TUE','WED','THU','FRI','SAT','SUN']
+        
+        self.ch_week_1 = timedelta(hours=9,minutes=06)
+        self.ch_week_2 = timedelta(hours=7,minutes=36)
+        
+        self.week_day_ch = [self.ch_week_1,self.ch_week_1,self.ch_week_1,self.ch_week_1,self.ch_week_2,timedelta(0),timedelta(0)]
 
         '''
         self.il = wx.ImageList(16, 16)
@@ -23,16 +35,18 @@ class CReportVirtualList(wx.ListCtrl):
         self.SetImageList(self.il, wx.IMAGE_LIST_SMALL)
         '''
         self.InsertColumn(0, "Date", wx.LIST_FORMAT_LEFT, -1) 
-        self.InsertColumn(1, "m1", wx.LIST_FORMAT_LEFT, -1)
-        self.InsertColumn(2, "m2", wx.LIST_FORMAT_LEFT, -1)
-        self.InsertColumn(3, "m3", wx.LIST_FORMAT_LEFT, -1)
-        self.InsertColumn(4, "m4", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(1, "week", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(2, "m1", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(3, "m2", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(4, "m3", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(5, "m4", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(6, "CH", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(7, "CH-ref", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(8, "HE", wx.LIST_FORMAT_LEFT, -1)
+        self.InsertColumn(9, "CP", wx.LIST_FORMAT_LEFT, -1)
 
         self.Report_date_list = []
-        self.Report_date_list.append("09/01/2011")
-        self.Report_date_list.append("10/01/2011")
-        self.Report_date_list.append("11/01/2011")
-                
+
         self.SetItemCount(len(self.Report_date_list))
         
         '''
@@ -57,20 +71,95 @@ class CReportVirtualList(wx.ListCtrl):
     # on values from some external data source, but for
     # this demo we'll just calculate them
     def OnGetItemText(self, item, col):
-        if col == 0:
-            return self.Report_date_list[item]
+        if len(self.Report_date_list) > 0:
+            if col == 0:
+                return self.Report_date_list[item]['DATE'].strftime("%d/%m/%Y")
+            elif col == 1:
+                return self.week_day[self.Report_date_list[item]['DATE'].weekday()]
+            elif col == 6:
+                return str(self.Report_date_list[item]['CH'])
+            elif col == 7:
+                return str(self.week_day_ch[self.Report_date_list[item]['DATE'].weekday()])
+            elif col == 8: #->HE
+                if self.Report_date_list[item]['CH'] > self.week_day_ch[self.Report_date_list[item]['DATE'].weekday()]:
+                    HE_delta = self.Report_date_list[item]['CH'] - self.week_day_ch[self.Report_date_list[item]['DATE'].weekday()]
+                else:    
+                    HE_delta = timedelta(0)
+                return str(HE_delta)
+            elif col == 9: #->CP
+                if self.Report_date_list[item]['CH'] < self.week_day_ch[self.Report_date_list[item]['DATE'].weekday()]:
+                    HE_delta = self.week_day_ch[self.Report_date_list[item]['DATE'].weekday()] - self.Report_date_list[item]['CH']
+                else:    
+                    HE_delta = timedelta(0)
+                return str(HE_delta)
+            elif col in [2,3,4,5]:
+                day_marks = self.Report_date_list[item]['MARKS']
+                if len(day_marks) > 0:
+                    imark = col - 2
+                    if imark < len(day_marks):
+                        return day_marks[imark].strftime("%H:%M:%S")
+                    else:
+                        return "00:00:00" #"%d (%d)"%(col,len(day_marks))
+                else:
+                    return "00:00:00"                
         else:
-            day_marks = self.cPontDB.GetDate(self.Report_date_list[item])
-            if col <= len(day_marks):
-                return "%02d:%02d:%02d" % (day_marks[col-1][0],day_marks[col-1][1],day_marks[col-1][2])
-            else:
-                return "00:00:00" #"%d (%d)"%(col,len(day_marks))
-
+            return " - "
     def OnGetItemImage(self, item):
         return -1
 
     def OnGetItemAttr(self, item):
         return None
+    def ApplyRange(self,start_date,end_date):
+        start_date_struct = time.strptime(start_date,"%d/%m/%Y")
+        end_date_struct = time.strptime(end_date,"%d/%m/%Y")
+        
+        cStart_date = date(start_date_struct.tm_year,start_date_struct.tm_mon,start_date_struct.tm_mday)
+        cEnd_date = date(end_date_struct.tm_year,end_date_struct.tm_mon,end_date_struct.tm_mday)
+        if cEnd_date > cStart_date:
+            delta = cEnd_date - cStart_date
+            del self.Report_date_list
+            self.Report_date_list = []
+            self.DeleteAllItems()
+
+            for iday in range(0,delta.days+1):
+                inter_date = cStart_date + timedelta(days=iday)
+                report_entry = {}
+                report_entry['DATE'] = inter_date
+                day_marks = self.cPontDB.GetDate(inter_date.strftime("%d/%m/%Y"))
+                time_mark_list = []
+                for mark in day_marks: 
+                    time_mark_list.append(datetime(inter_date.year,inter_date.month,inter_date.day,mark[0],mark[1],mark[2]))
+                time_mark_list.sort()
+                '''
+                for teste in time_mark_list:
+                    print teste
+                '''    
+                report_entry['MARKS'] = time_mark_list
+                #calculo da carga horaria
+                time_in = None
+                time_out = None
+                delta_time = timedelta(0)
+                for mark in time_mark_list:
+                    if time_in == None:
+                        time_in = mark
+                    elif time_out == None:
+                        time_out = mark
+                        delta_time = delta_time + (time_out-time_in)    
+                        time_in = None
+                        time_out = None
+                report_entry['CH'] = delta_time
+                #print delta_time 
+                self.Report_date_list.append(report_entry) 
+                #print 'outra data'
+                         
+            '''   
+            self.Report_date_list.append("09/01/2011")
+            self.Report_date_list.append("10/01/2011")
+            self.Report_date_list.append("11/01/2011")
+            '''       
+            self.SetItemCount(len(self.Report_date_list))
+            print len(self.Report_date_list)
+        
 
 class CReport(xrcCReportFrame):
     '''
@@ -90,6 +179,10 @@ class CReport(xrcCReportFrame):
         sizer.Add(self.cReportVirtual, 1, wx.EXPAND)
         self.CReportListCtrl.SetSizer(sizer)
         self.CReportListCtrl.SetAutoLayout(True)  
-        self.SetSize((400,350))
-              
+        self.SetSize((600,400))
+    def OnButton_CApplyButton(self, evt):
+        start_time_text = self.CStartTimeText.GetValue()
+        stop_time_text = self.CStopTimeText.GetValue()
+        if len(start_time_text) > 0 and len(stop_time_text) > 0:
+            self.cReportVirtual.ApplyRange(start_time_text,stop_time_text)     
         
