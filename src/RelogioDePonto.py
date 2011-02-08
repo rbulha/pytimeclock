16c8822b96d7324b2892a54659e5d7f3919806dd
rbulha <rbulha@gmail.com>
2011-02-08 10:26:16 +0000

    CDialogMark class moved from RelogioDePonto to a separated module.

--- src/RelogioDePonto.py
+++ src/RelogioDePonto.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
 import wx
-import time
 
 from GUI_xrc import xrcCRelogioFrame
-from GUI_xrc import xrcCMarkDlg
 
+from DialogMark import CDialogMark
 from PontoDB            import CPontoDB
 from BookMark           import CColoredGauge
 from Milestones         import CMilestonePanel
@@ -75,109 +74,7 @@ class CTrayBar(wx.TaskBarIcon):
         menu.Append(self.ID_exit, "Exit")   
         return menu
 
-class CDialogMark(xrcCMarkDlg):
-    def __init__(self,parent):
-        xrcCMarkDlg.__init__(self, parent)
-        time_stamp = time.localtime()
-        self.cTimeStamp.SetValue(time.strftime("%H:%M:%S %d/%m/%Y",time_stamp))
-        self.wxTimeSliderHour.SetValue(time_stamp.tm_hour)
-        self.wxTimeSlider.SetValue(time_stamp.tm_min)
-        self.wxTimeSliderSec.SetValue(time_stamp.tm_sec)
-        self.wxTimeSliderDay.SetValue(time_stamp.tm_mday)
-        self.wxTimeSliderMonth.SetValue(time_stamp.tm_mon)
-        self.wxTimeSliderYear.SetValue(time_stamp.tm_year)
-        self.wxTimeSliderYear.SetRange(time_stamp.tm_year-50,time_stamp.tm_year+50)
         
-    def OnButton_CMarkBtn(self, evt):
-        print '[CDialogMark] - Type:',self.CRadio_Type.GetSelection()
-        print '[CDialogMark] - subType:',self.CRadio_subType.GetSelection() 
-        print '[CDialogMark] - comment:',self.CCommentTextCtrl.GetValue()
-        self.EndModal(self.CRadio_Type.GetSelection()|self.CRadio_subType.GetSelection())
-    def OnButton_CCancelBtn(self, evt):
-        self.EndModal(-1)
-    def OnClose(self, evt):
-        self.EndModal(-1)    
-    def GetMark(self):
-        tipo = ['ENTRADA','SAIDA']
-        sub  = ['NORMAL','ALMOCO','EXTRAORDINARIA']
-        return (tipo[self.CRadio_Type.GetSelection()],   \
-                sub[self.CRadio_subType.GetSelection()], \
-                self.CCommentTextCtrl.GetValue(),        \
-                time.strptime(self.cTimeStamp.GetValue(),"%H:%M:%S %d/%m/%Y"))
-    def OnScroll_wxTimeSlider(self, evt):
-        #print '[CDialogMark][OnScroll_wxTimeSlider] - evt: ',evt.GetPosition()  
-        time_stamp = time.strptime(self.cTimeStamp.GetValue(),"%H:%M:%S %d/%m/%Y")
-        #time_stamp[4] = evt.GetPosition()
-        time_stamp_aux = (time_stamp.tm_year,
-                          time_stamp.tm_mon,
-                          time_stamp.tm_mday,
-                          time_stamp.tm_hour,
-                          evt.GetPosition(),
-                          time_stamp.tm_sec,
-                          time_stamp.tm_wday,
-                          time_stamp.tm_yday,
-                          time_stamp.tm_isdst)
-        self.cTimeStamp.SetValue(time.strftime("%H:%M:%S %d/%m/%Y",time_stamp_aux))
-    def OnScroll_wxTimeSliderHour(self, evt):
-        time_stamp = time.strptime(self.cTimeStamp.GetValue(),"%H:%M:%S %d/%m/%Y")
-        time_stamp_aux = (time_stamp.tm_year,
-                          time_stamp.tm_mon,
-                          time_stamp.tm_mday,
-                          evt.GetPosition(),
-                          time_stamp.tm_min,
-                          time_stamp.tm_sec,
-                          time_stamp.tm_wday,
-                          time_stamp.tm_yday,
-                          time_stamp.tm_isdst)
-        self.cTimeStamp.SetValue(time.strftime("%H:%M:%S %d/%m/%Y",time_stamp_aux))        
-    def OnScroll_wxTimeSliderSec(self, evt):
-        time_stamp = time.strptime(self.cTimeStamp.GetValue(),"%H:%M:%S %d/%m/%Y")
-        time_stamp_aux = (time_stamp.tm_year,
-                          time_stamp.tm_mon,
-                          time_stamp.tm_mday,
-                          time_stamp.tm_hour,
-                          time_stamp.tm_min,
-                          evt.GetPosition(),
-                          time_stamp.tm_wday,
-                          time_stamp.tm_yday,
-                          time_stamp.tm_isdst)
-        self.cTimeStamp.SetValue(time.strftime("%H:%M:%S %d/%m/%Y",time_stamp_aux))        
-    def OnScroll_wxTimeSliderDay(self, evt):
-        time_stamp = time.strptime(self.cTimeStamp.GetValue(),"%H:%M:%S %d/%m/%Y")
-        time_stamp_aux = (time_stamp.tm_year,
-                          time_stamp.tm_mon,
-                          evt.GetPosition(),
-                          time_stamp.tm_hour,
-                          time_stamp.tm_min,
-                          time_stamp.tm_sec,
-                          time_stamp.tm_wday,
-                          time_stamp.tm_yday,
-                          time_stamp.tm_isdst)
-        self.cTimeStamp.SetValue(time.strftime("%H:%M:%S %d/%m/%Y",time_stamp_aux))        
-    def OnScroll_wxTimeSliderMonth(self, evt):
-        time_stamp = time.strptime(self.cTimeStamp.GetValue(),"%H:%M:%S %d/%m/%Y")
-        time_stamp_aux = (time_stamp.tm_year,
-                          evt.GetPosition(),
-                          time_stamp.tm_mday,
-                          time_stamp.tm_hour,
-                          time_stamp.tm_min,
-                          time_stamp.tm_sec,
-                          time_stamp.tm_wday,
-                          time_stamp.tm_yday,
-                          time_stamp.tm_isdst)
-        self.cTimeStamp.SetValue(time.strftime("%H:%M:%S %d/%m/%Y",time_stamp_aux))        
-    def OnScroll_wxTimeSliderYear(self, evt):
-        time_stamp = time.strptime(self.cTimeStamp.GetValue(),"%H:%M:%S %d/%m/%Y")
-        time_stamp_aux = (evt.GetPosition(),
-                          time_stamp.tm_mon,
-                          time_stamp.tm_mday,
-                          time_stamp.tm_hour,
-                          time_stamp.tm_min,
-                          time_stamp.tm_sec,
-                          time_stamp.tm_wday,
-                          time_stamp.tm_yday,
-                          time_stamp.tm_isdst)
-        self.cTimeStamp.SetValue(time.strftime("%H:%M:%S %d/%m/%Y",time_stamp_aux))        
                      
 class CCRelogioFrame(xrcCRelogioFrame):
     def __init__(self, parent):
